5c2cc0f23700e306c4f5485597e14b2e3af07368
Mridul Roy <midlry.mr@gmail.com>
2024-12-10 05:53:23 +0530

    feat: add database microservice with initial configuration and model

---
+++ database/src/main/java/io/d4rkr0n1n/database/model/Note.java
@@ -0,0 +1,23 @@
+package io.d4rkr0n1n.database.model;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Note {
+  @Id UUID id;
+  String name;
+  String contents;
+  Timestamp timestamp;
+}
